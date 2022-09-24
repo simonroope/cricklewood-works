b6d38ea3ef8550133eba29007244ca309b131654
Simon Roope <simonroope@gmail.com>
2022-09-24 14:47:21 +0100

    gallery wip

--- client/src/components/PartDetail.js
+++ client/src/components/PartDetail.js
@@ -11,50 +11,24 @@ const PartDetail = ({ part, setPart }) => {
 
   const [images, setImages] = useState([]);
 
-  let xx;
-  const imgDefault = [ { original: '', thumbnail: '' } ]
-
-  const imgTest = [
-    {
-      original: `${baseUrl}bafkreibaap3gv7cx2pgqyvpz3ag556hki4nkdvgb2ma2zafsii3dwb4niy`,
-      thumbnail: `${baseUrl}bafkreibaap3gv7cx2pgqyvpz3ag556hki4nkdvgb2ma2zafsii3dwb4niy`
-    },
-    {
-      original: `${baseUrl}bafkreih7u5yo2ypqrficaa5tvujn7ni34g26jhav43w2avabrjchrk53dq`,
-      thumbnail: `${baseUrl}bafkreih7u5yo2ypqrficaa5tvujn7ni34g26jhav43w2avabrjchrk53dq`
-    },
-    {
-      original: `${baseUrl}bafkreiesxfgdcddmldtrfcl5c2oqo2gc6xgjrexspzllovhcwgafngo2qe`,
-      thumbnail: `${baseUrl}bafkreiesxfgdcddmldtrfcl5c2oqo2gc6xgjrexspzllovhcwgafngo2qe`
-    },
-    {
-      original: `${baseUrl}bafybeidn6jpu36o57k75vq354glhfk3a3iiawggvuwufxq26r6i65dxwim`,
-      thumbnail: `${baseUrl}bafybeidn6jpu36o57k75vq354glhfk3a3iiawggvuwufxq26r6i65dxwim`
-    },
-    {
-      original: `${baseUrl}bafkreig3mddorcfckcm77nywwaihyr3g2vn3vgjg3m5gsmyncrl55lsp7q`,
-      thumbnail: `${baseUrl}bafkreig3mddorcfckcm77nywwaihyr3g2vn3vgjg3m5gsmyncrl55lsp7q`
-    },
-  ];
-  console.log('imgTest: ', imgTest);
-
   console.log('part: ', part);
 
   useEffect(() => {
     const init = async () => {
 
-      const details = await getPartDetails(12);
+      const details = await getPartDetails(9);
       //console.log('details: ', details);
       
-      // const detailImages = details.map(e => e.part_photos); 
       const detailImages = details[0].part_photos.split(',')
       //console.log('detailImages: ', detailImages);
      
-      xx = detailImages.map(url => ({ original: `${baseUrl}${url}`, thumbnail: `${baseUrl}${url}` }));
-      console.log('xx: ', xx);
-      
-      setImages( detailImages.map(url => ({ original: `${baseUrl}${url}`, thumbnail: `${baseUrl}${url}` })))
-      console.log('images: ', images);
+      // Requires assignment to local variable as useState state variable is not updated before div is rendered => undefined 
+      // setImages( detailImages.map(url => ({ original: `${baseUrl}${url}`, thumbnail: `${baseUrl}${url}` })))
+      const galleryImages = detailImages.map(url => ({ original: `${baseUrl}${url}`, thumbnail: `${baseUrl}${url}` }));
+    
+      setImages(galleryImages) 
+      //console.log('images: ', images);
+     
     }
 
     init()
@@ -64,7 +38,7 @@ const PartDetail = ({ part, setPart }) => {
 
     <div className="flex p-10">
       <div className="w-1/2">
-        <ImageGallery items={ imgTest } />
+        <ImageGallery items={ images } />
       </div>
       <div className="w-1/3 m-5">
         PartDetail Text
